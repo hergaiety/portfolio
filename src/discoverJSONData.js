0df66853e77602aecbebef18862b3fc08b0fc13f
Joe Wroten <joe@wroten.me>
2018-12-30 23:31:45 -0600

    Stubbed homepage with links to other pages

--- src/discoverJSONData.js
+++ src/discoverJSONData.js
@@ -1,5 +1,6 @@
 const { join } = require('path');
 const { readdir, readJson } = require('fs-extra');
+const marked = require('marked');
 const requestGithub = require('./request');
 const schema = require('./schema');
 
@@ -15,7 +16,12 @@ const getPinnedRepoJSONs = async () => {
       }
     }
   } = await requestGithub(schema);
-  return pinnedRepoData.map(data => data.node);
+  return pinnedRepoData.map(data => {
+    if (data.node.readme && data.node.readme.text) {
+      data.node.readme.html = marked(data.node.readme.text);
+    }
+    return data.node;
+  });
 };
 
 const getStaticPageJSONs = async () => {
@@ -35,7 +41,13 @@ const getStaticPageJSONs = async () => {
   });
 
   try {
-    return await Promise.all(readFilesPromises);
+    let allData = await Promise.all(readFilesPromises);
+    return allData.map(data => {
+      if (data.readme && data.readme.text) {
+        data.readme.html = marked(data.readme.text);
+      }
+      return data;
+    });
   } catch (error) {
     console.error('Error while reading JSON', error);
     return;
--- src/generateHTMLPages.js
+++ src/generateHTMLPages.js
@@ -1,22 +1,24 @@
 const { join } = require('path');
 const { outputFile } = require('fs-extra');
-const marked = require('marked');
 const getTemplate = require('./template');
 
 const distPath = './dist';
 
 const generateIndexPage = async jsonData => {
-  console.log('Index page...TODO');
+  const indexTemplate = await getTemplate('index');
+
+  try {
+    return outputFile(join(distPath, 'index') + '.html', indexTemplate(jsonData));
+  } catch (error) {
+    console.error('Error while writing index html file', error);
+    return;
+  }
 };
 
 const generateInteriorPages = async jsonData => {
   const interiorTemplate = await getTemplate('interior');
   const saveAsInteriorHTML = async json => {
-    let fileName = json.name.replace(/[^a-zA-Z\d:]/g, '').toLowerCase();
-    if (json.readme && json.readme.text) {
-      json.readme.html = marked(json.readme.text);
-    }
-    return outputFile(join(distPath, fileName) + '.html', interiorTemplate(json));
+    return outputFile(join(distPath, json.name) + '.html', interiorTemplate(json));
   }
 
   try {
