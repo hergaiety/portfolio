36d77bfe228defff4bf0d324ecdeada1e13ef715
Joe Wroten <joe@wroten.me>
2019-01-25 10:47:33 -0600

    Fixed race condition for partials

--- src/template.js
+++ src/template.js
@@ -5,13 +5,15 @@ const { join, basename } = require('path');
 const registerPartials = async () => {
   try {
     let results = await readdir('./src/templates/partials')
-    results.forEach(async filename => {
+    let resultsPromises = results.map(async filename => {
       let filePath = join('./src/templates/partials', filename);
       let partialName = basename(filename, '.hbs');
       let partialContents = await readFile(filePath);
       partialContents = partialContents.toString();
       Handlebars.registerPartial(partialName, partialContents);
     });
+
+    return Promise.all(resultsPromises);
   } catch(error) {
     console.error('Could not read partials in ./src/templates/partials/', error);
     return;
